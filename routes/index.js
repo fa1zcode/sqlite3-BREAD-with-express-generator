f55a99a91c0a1ddd6d73792adabfcad4095c5dcb
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-02-24 12:00:34 +0700

    login, logout, and register done

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,15 @@ var path = require("path");
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(path.join(__dirname, "..", "db", "todo.db"));
 
+function isLoggedIn(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect("/login");
+    console.log("coba lagi");
+  }
+}
+
 router.get("/login", function (req, res) {
   res.render("login");
 });
@@ -11,26 +20,53 @@ router.get("/login", function (req, res) {
 router.post("/login", function (req, res) {
   const email = req.body.email;
   const password = req.body.password;
-  
-  console.log(email)
-  console.log(password)
 
   db.get(
     "select * from user where email = ? and password = ?",
     [email, password],
-    (err, raw) => {
+    (err, user) => {
       if (err) return res.send("Login Gagal");
-      if (!raw) return res.send("Masukkan Username")
-      res.redirect("/")
+      if (!user) return res.send("Email / Password yang anda masukkan salah");
+      //console.log(user);
+      req.session.user = user;
+      console.log(req.session.user);
+      res.redirect("/");
     }
   );
 });
 
-router.get("/", function (req, res) {
+router.get("/register", function (req, res) {
+  res.render("register");
+});
+
+router.post("/register", function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+  const fullname = req.body.fullname;
+
+  db.run(
+    "insert into user (email, password, fullname) values (?,?,?)",
+    [email, password, fullname],
+    (err, user) => {
+      if (err) return res.send("Register Failed");
+      res.redirect("/login");
+    }
+  );
+});
+
+router.get("/logout", function (req, res) {
+  req.session.destroy(function (err) {
+    // cannot access session here
+    res.render("login");
+  });
+});
+
+router.get("/", isLoggedIn, function (req, res) {
   const url = req.url == "/" ? "/?page=1" : req.url;
   console.log(url);
 
   const params = [];
+  params.push(`userid = ${req.session.user.id}`);
 
   if (req.query.task) {
     params.push(`task like '${req.query.task}' `);
@@ -66,26 +102,31 @@ router.get("/", function (req, res) {
         jumlahHalaman,
         query: req.query,
         url,
+        user: req.session.user,
       });
     });
   });
 });
 
-router.get("/add", function (req, res) {
+router.get("/add", isLoggedIn, function (req, res) {
   res.render("add");
 });
 
 router.post("/add", function (req, res) {
   task = req.body.task;
   // query binding = use (?) to prevent hack via sql injection
-  db.run("insert into todo(task) values (?) ", [task], (err, raws) => {
-    if (err) return res.send(err);
-    console.log(raws);
-    res.redirect("/");
-  });
+  db.run(
+    "insert into todo(task, userid) values (?,?) ",
+    [task, req.session.user.id],
+    (err, raws) => {
+      if (err) return res.send(err);
+      console.log(raws);
+      res.redirect("/");
+    }
+  );
 });
 
-router.get("/delete/:id", function (req, res) {
+router.get("/delete/:id", isLoggedIn, function (req, res) {
   const id = Number(req.params.id);
   db.run("delete from todo where id = ? ", [id], (err, raws) => {
     if (err) return res.send(err);
@@ -94,7 +135,7 @@ router.get("/delete/:id", function (req, res) {
   });
 });
 
-router.get("/edit/:id", function (req, res) {
+router.get("/edit/:id", isLoggedIn, function (req, res) {
   const id = Number(req.params.id);
   db.get("select * from todo where id = ?", [id], (err, raws) => {
     console.log(err);
@@ -103,7 +144,7 @@ router.get("/edit/:id", function (req, res) {
   });
 });
 
-router.post("/edit/:id", function (req, res) {
+router.post("/edit/:id", isLoggedIn, function (req, res) {
   const id = Number(req.params.id);
   task = req.body.task;
   complete = JSON.parse(req.body.complete);
